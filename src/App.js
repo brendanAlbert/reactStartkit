c7209ae538058e2b725190232cdf6fc600f81f92
brendan <brendan@brendans-MacBook-Pro.local>
2019-09-10 17:51:48 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './components/App.scss';
+
+const App = ({ title }) => (
+    <>
+        <h1>( ͡° ͜ʖ ͡°)</h1>
+        <div>{title}</div>
+    </>
+)
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // creates style nodes from JS strings
+                    'style-loader',
+                    // translates css into commonJS
+                    'css-loader',
+                    // compiles sass to css
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
